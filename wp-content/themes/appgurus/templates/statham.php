157af63ed41d60ee9b54d25cded5bc112b8cec10
ajaytrilokn <ajay.metaliya@trilokninfotech.com>
2024-06-03 17:36:43 +0530

    Add H1 Tag

--- wp-content/themes/appgurus/templates/statham.php
+++ wp-content/themes/appgurus/templates/statham.php
@@ -5,7 +5,11 @@ Template Name:Statham Template
 get_header();
 ?>
 <div class="statham">
-
+<section id="statham">
+    <section class="container">
+        <h1>Hello Jason Statham</h1>
+    </section>
+</section>
 </div>
 <?php 
 get_footer()
